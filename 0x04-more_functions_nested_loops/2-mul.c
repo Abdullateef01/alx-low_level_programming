e5773d80cc01a4b39ad585618ba4651dfef70446
Abdullateef01 <lateefhassan29@gmail.com>
2022-09-16 16:42:42 -0700

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - print multiplucation of character a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
