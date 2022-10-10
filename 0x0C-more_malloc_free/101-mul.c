4120dde47bdc1865bcee53e52cb6e7d96ecf8a94
Abdullateef01 <lateefhassan29@gmail.com>
2022-10-10 14:26:07 -0700

    malloc

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
+ */ 
+int main(int argc, char *argv[])
+{
+	char *final_prod, *next_prod;
+	int size, index, digit, zeroes = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (*(argv[1]) == '0')
+		argv[1] = iterate_zeroes(argv[1]);
+	if (*(argv[2]) == '0')
+		argv[2] = iterate_zeroes(argv[2]);
+	if (*(argv[1]) == '\0' || *(argv[2]) == '\0')
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	size = find_len(argv[1]) + find_len(argv[2]);
+	final_prod = create_xarray(size + 1);
+	next_prod = create_xarray(size + 1);
